fe7d4c2cd5afeae8b0e077ef674c974033edb92a
Fernando <fernando.k.lozano@gmail.com>
2022-07-15 22:18:14 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const navLinks = document.querySelectorAll(".nav-link");
+
+navLinks.forEach(link => {
+  link.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    // toggle active
+    // toggle last active
+    // point last active
+    // change number operator visually and programmatically
+        // on change, reset calculator
+  });
+});
